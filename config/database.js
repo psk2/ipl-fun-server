712a78bbd0b217e8e6664e5069e28a921e079269
psk2 <sai.krishna@isteer.in>
2018-02-16 17:18:54 +0530

    integrated with the api

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === "production") {
-    module.exports = { mongoURI: "mongodb://psk:root@ds131698.mlab.com:31698/rest-shop" }
+    module.exports = { mongoURI: "mongodb://psk:root@ds239128.mlab.com:39128/ipl-fun-server" }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/ipl-fun-server" }
 }
